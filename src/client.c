1895a459b4ef53d6e0992b8ca98832022d851e55
Scentle5S <nicolas.bfd@gmail.com>
2015-06-10 21:19:18 +0200

    Maj client.c

--- src/client.c
+++ src/client.c
@@ -9,31 +9,35 @@
 #include "client.h"
 
 /**
- * \fn void constructionRequete(Requete *req, Voiture *v, int croisement_precedent, int croisement_precedent_orientation, int croisement, int croisement_orientation, int voie, int traverse, int type)
- * \brief Construit une requete pour le serveur.
- * La requete peut prendre 5 formes :
- * - Information d'arrivee : une voiture arrive par la voie mentionnee
- * - Demande de traverser : une voiture demande a traverser une zone critique (<=> un croisement avec une autre voie)
- * - Information de debut de traversement : une voiture est en train de traverser une zone critique
- * - Information de fin de traversement : une voiture a fini de traverser une zone critique
- * - Information de depart : une voiture sort par la voie mentionnee
+ * \fn void constructionRequete(Requete *req, Voiture *v, int carrefour, int croisement_precedent, int croisement_precedent_orientation, int croisement, int croisement_orientation, int voie, int traverse, int type)
+ * \brief Construit une requete pour le carrefour, qui sera eventuellement retransmise au serveur si necessaire.
+ *
+ * La requete peut prendre 4 formes :
+ * - MESSARRIVE : message d'information d'arrivee sur un carrefour. N'implique aucun traitement, affiche juste l'information.
+ * - MESSDEMANDE : demande d'arrivee en zone avant, pendant, ou apres un croisement. Implique une demande au serveur (depuis le carrefour) qui va analyser le carrefour pour prendre une decision.
+ * - MESSINFO : message d'information d'arrivee avant, pendant, ou apres un croisement. N'implique aucun traitement, affiche juste l'information.
+ * - MESSSORT : message d'information de sortie d'un carrefour. Implique de mettre a jour la file d'attente "apres" le croisement precedent.
  *
  * \param req Pointeur vers la requete qui va etre renseignee.
  * \param v Pointeur vers la voiture effectuant la requete.
- * \param carrefour Numero du carrefour ou se trouve la voiture effectuant la requete.
- * \param croisement_precedent L'indice de la zone critique (<=> le croisement) precedemment visitee le cas echeant (utile lorsque la voiture quitte un croisement pour modifier ce dernier).
- * \param croisement_precedent_orientation L'orientation (horizontal / vertical) du croisement precedent (utile pour mettre a jour les files d'attente du croisement precedent quand une voiture arrive a un nouveau croisement).
+ * \param croisement_precedent L'indice de la zone critique (<=> le croisement) precedemment visitee le cas echeant (utile lorsque la voiture quitte un croisement pour modifier le precedent).
+ * \param croisement_precedent_orientation L'orientation (horizontal / vertical) du croisement precedent (utile pour mettre a jour les files d'attente du croisement precedent quand une voiture arrive a un nouveau croisement). Peut valoir :
+ * - HO : horizontal.
+ * - VE : vertical.
  * \param croisement L'indice de la zone critique (<=> le croisement) le cas echeant.
- * \param croisement_orientation L'orientation (horizontal / vertical) du croisement (utile pour mettre a jour les files d'attente du croisement).
+ * \param croisement_orientation L'orientation (horizontal / vertical) du croisement (utile pour mettre a jour les files d'attente du croisement). Peut valoir :
+ * - HO : horizontal.
+ * - VE : vertical.
  * \param voie L'indice de la voie d'arrivee, de depart, ou de croisement (<=> la voie qui croise avec celle de la voiture a l'endroit critique).
  * \param traverse L'etat de traversement (directement lie aux informations de traversement). Peut valoir :
- * - AVANT
- * - PENDANT
- * - APRES
+ * - AVANT : avant la zone critique (file de voitures avant le croisement).
+ * - PENDANT : pendant la zone critique (le croisement en lui meme).
+ * - APRES : apres la zone critique (file de voitures apres le croisement).
  * \param type Le type de requete. Peut valoir :
- * - MESSARRIVE
- * - MESSINFO
- * - MESSSORT
+ * - MESSARRIVE.
+ * - MESSDEMANDE.
+ * - MESSINFO.
+ * - MESSSORT.
  */
 void constructionRequete(Requete *req, Voiture *v, int carrefour, int croisement_precedent, int croisement_precedent_orientation, int croisement, int croisement_orientation, int voie, int traverse, int type)
 {
@@ -53,7 +57,7 @@ void constructionRequete(Requete *req, Voiture *v, int carrefour, int croisement
  * \fn void affichageRequete(Requete *req)
  * \brief Affiche la nature et les informations de la requete.
  *
- * \param req La requete a afficher
+ * \param req Pointeur sur la requete a afficher.
  */
 void affichageRequete(Requete *req)
 {
